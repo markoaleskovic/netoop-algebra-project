bf097d7ded1a457d7b40ef8d8a9a48f94a34449e
markoaleskovic <malesko@lagebra.hr>
2025-06-16 11:53:34 +0200

    Fixed last commit

---
+++ DataLayer/Models/Match.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.Models
+{
+	public class Match
+	{
+		public string? Venue { get; set; }
+		public string? Location { get; set; }
+		public string? Status { get; set; }
+		public string? Time { get; set; }
+		public string? Fifa_Id { get; set; }
+		public Weather? Weather { get; set; }
+		public string? Attendance { get; set; }
+		public List<string>? Officials { get; set; }
+		public string? Stage_Name { get; set; }
+		public string? Home_Team_Country { get; set; }
+		public string? Away_Team_Country { get; set; }
+		public DateTime? Datetime { get; set; }
+		public string? Winner { get; set; }
+		public string? Winner_Code { get; set; }
+		public TeamScore? Home_Team { get; set; }
+		public TeamScore? Away_Team { get; set; }
+		public List<Event>? Home_Team_Events { get; set; }
+		public List<Event>? Away_Team_Events { get; set; }
+		public TeamStatistics? Home_Team_Statistics { get; set; }
+		public TeamStatistics? Away_Team_Statistics { get; set; }
+		public DateTime? Last_Event_Update_At { get; set; }
+		public DateTime? Last_Score_Update_At { get; set; }
+	}
+
+	public class TeamScore
+	{
+		public string? Country { get; set; }
+		public string? Code { get; set; }
+		public int? Goals { get; set; }
+		public int? Penalties { get; set; }
+	}
+}
---
+++ DataLayer/Models/MatchScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+	public class MatchScore
+	{
+		public Team? LeftTeam { get; set; }
+		public Team? RightTeam { get; set; }
+		public int? LeftScore { get; set; }
+		public int? RightScore { get; set; }
+	}
+}
